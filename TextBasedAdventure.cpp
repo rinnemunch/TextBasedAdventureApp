719ad5f110db36d5c322a5527943adaabfa562b2
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-10 07:16:27 -0400

    feat: add main menu loop and dungeon entry

--- TextBasedAdventure.cpp
+++ TextBasedAdventure.cpp
@@ -1,20 +1,17 @@
-// TextBasedAdventure.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream> 
+using namespace std; 
 
-#include <iostream>
+int main() {
+	bool gameRunning = true; 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	while (gameRunning) {
+		cout << "\n============================= \n"; 
+		cout << "   WELCOME TO DUNGEON QUEST\n"; 
+		cout << "=============================\n"; 
+		cout << "1. Enter the Dungeon\n"; 
+		cout << "2. Quit Game \n"; 
+		cout << "Choose an option: "; 
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
