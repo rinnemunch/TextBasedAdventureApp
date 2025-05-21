4f675ec2a9a46b594208899782be4ee55793dc62
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-21 04:28:13 -0400

    feat: add currentRoom variable to track player location

--- TextBasedAdventure.cpp
+++ TextBasedAdventure.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	bool gameRunning = true; 
+	string currentRoom = "Entrance"; 
 
 	while (gameRunning) {
 		cout << "\n============================= \n"; 
@@ -16,10 +17,12 @@ int main() {
 		cin >> choice; 
 
 		if (choice == 1) {
+			currentRoom = "Entrance"; 
 			cout << "\nYou step into the dungeon... it's dark and cold.\n"; 
+			cout << "Current Room: " << currentRoom << endl;
 		}
 		else if (choice == 2) {
-			cout << "\nThanks  for playing!\n"; 
+			cout << "\nThanks for playing!\n"; 
 			gameRunning = false; 
 		}
 		else {
