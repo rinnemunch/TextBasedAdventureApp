02bd715909591a89d3f1982d1e123363d9569056
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-21 04:41:58 -0400

    feat: add Entrance room logic with movement options

--- TextBasedAdventure.cpp
+++ TextBasedAdventure.cpp
@@ -1,34 +1,63 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
 int main() {
-	bool gameRunning = true; 
-	string currentRoom = "Entrance"; 
+    bool gameRunning = true;
+    string currentRoom = "Menu";
 
-	while (gameRunning) {
-		cout << "\n============================= \n"; 
-		cout << "   WELCOME TO DUNGEON QUEST\n"; 
-		cout << "=============================\n"; 
-		cout << "1. Enter the Dungeon\n"; 
-		cout << "2. Quit Game \n"; 
-		cout << "Choose an option: "; 
+    while (gameRunning) {
 
-		int choice; 
-		cin >> choice; 
+        if (currentRoom == "Menu") {
+            cout << "\n=============================\n";
+            cout << "   WELCOME TO DUNGEON QUEST\n";
+            cout << "=============================\n";
+            cout << "1. Enter the Dungeon\n";
+            cout << "2. Quit Game\n";
+            cout << "Choose an option: ";
 
-		if (choice == 1) {
-			currentRoom = "Entrance"; 
-			cout << "\nYou step into the dungeon... it's dark and cold.\n"; 
-			cout << "Current Room: " << currentRoom << endl;
-		}
-		else if (choice == 2) {
-			cout << "\nThanks for playing!\n"; 
-			gameRunning = false; 
-		}
-		else {
-			cout << "\nInvalid choice. Try again.\n"; 
-		}
-	}
+            int choice;
+            cin >> choice;
 
-	return 0;
+            if (choice == 1) {
+                currentRoom = "Entrance";
+                cout << "\nYou step into the dungeon... it's dark and cold.\n";
+                cout << "Current Room: " << currentRoom << endl;
+            }
+            else if (choice == 2) {
+                cout << "\nThanks for playing!\n";
+                gameRunning = false;
+            }
+            else {
+                cout << "\nInvalid choice. Try again.\n";
+            }
+        }
+
+        else if (currentRoom == "Entrance") {
+            cout << "\nYou are standing in the dungeon entrance.\n";
+            cout << "1. Go left to the Armory\n";
+            cout << "2. Go right to the Hallway\n";
+            cout << "3. Exit to Main Menu\n";
+            cout << "Choose an option: ";
+
+            int choice;
+            cin >> choice;
+
+            if (choice == 1) {
+                currentRoom = "Armory";
+                cout << "\nYou enter the Armory. Rusty weapons line the walls.\n";
+            }
+            else if (choice == 2) {
+                currentRoom = "Hallway";
+                cout << "\nYou walk into a long, shadowy hallway.\n";
+            }
+            else if (choice == 3) {
+                currentRoom = "Menu";
+            }
+            else {
+                cout << "\nInvalid choice. Try again.\n";
+            }
+        }
+    }
+
+    return 0;
 }
